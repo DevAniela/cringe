b070ee910aa8f69ff6d72e3759e8473ed468c96f
DevAniela <dismal.damsel@gmail.com>
2025-11-19 12:03:07 +0200

    Implement store method in MovieController.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class MovieController extends Controller
 {
@@ -17,4 +20,25 @@ class MovieController extends Controller
     {
         return view('movies.create');
     }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255|unique:movies,title',
+            'release_year' => 'required|integer|min:1900|max: ' . date('Y'),
+            'poster_url' => 'nullable|url|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Movie::create([
+            'title' => $validated['title'],
+            'release_year' => $validated['release_year'],
+            'poster_url' => $validated['poster_url'] ?? null,
+            'description' => $validated['description'] ?? null,
+            'user_id' => Auth::id(),
+        ]);           
+            
+            return redirect()->route('movies.index')->with('success', 'Movie added!');
+    }
+
 }
